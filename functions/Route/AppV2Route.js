9f23abfb397413dd1240453bf3c95c91cbd33aaf
Kim HyunWoo <stories2@naver.com>
2018-12-19 17:09:19 +0900

    AppV2Route : fix json convert failure

--- functions/Route/AppV2Route.js
+++ functions/Route/AppV2Route.js
@@ -1,6 +1,7 @@
 exports.hellowWorld = function (request, response) {
 
-    console.log("v2 hello world: " + JSON.stringify(request))
+    console.log("v2 hello world headers: " + JSON.stringify(request.headers))
+    console.log("v2 hello world body: " + JSON.stringify(request.body))
 
     var responseMessage = {
         "version": "2.0",
